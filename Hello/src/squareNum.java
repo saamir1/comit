bbe3e222ca0a2714289e3dcb2d25f1764e03382e
saamir <aamir_sobia@yahoo.com>
2019-07-17 17:54:26 -0600

    inital commit

---
+++ Hello/src/squareNum.java
@@ -0,0 +1,12 @@
+public class squareNum {
+    public static void main(String[] args) {
+        int sq = 0;
+        int b = 1;
+        System.out.println("square of number from 1 to 100");
+        while(b < 20){
+            System.out.println(sq + "");
+            sq = b * b;
+            b++;
+        }
+    }
+}
