c46900319b5db81713b22c52d4e43466d7288c82
saamir <aamir_sobia@yahoo.com>
2019-07-17 18:01:57 -0600

    firstcommit

---
+++ Hello/src/VehicleTest.java
@@ -0,0 +1,12 @@
+public class VehicleTest {
+    public static void main(String[] args) {
+
+
+        Ex18_Vehicle v1 = new Ex18_Vehicle("vehicle#1",100000, "family ");
+        System.out.println(v1.toString());
+
+        Ex18_Vehicle v2 = new Ex18_Vehicle("vehicle#2", 100000, "public transportation");
+        System.out.println(v2.toString());
+
+    }
+}
